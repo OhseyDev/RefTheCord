7a3e4df8b29ecd8027f1f2921dd78b3ba3feabf3
Ohsey <90527387+OhseyDev@users.noreply.github.com>
2023-06-15 11:35:17 +0100

    Start work on bot

---
+++ go.mod
@@ -0,0 +1,14 @@
+module DiscordRefereeBot/m/v2
+
+go 1.20
+
+require (
+	github.com/bwmarrin/discordgo v0.27.1
+	github.com/mattn/go-sqlite3 v1.14.17
+)
+
+require (
+	github.com/gorilla/websocket v1.5.0 // indirect
+	golang.org/x/crypto v0.10.0 // indirect
+	golang.org/x/sys v0.9.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bwmarrin/discordgo"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var (
+	GuildID  = flag.String("guild", "", "Test guild ID. If not passed - bot registers commands globally")
+	BotToken = flag.String("token", "", "Bot access token")
+)
+
+var s *discordgo.Session
+
+func init() { flag.Parse() }
+
+func init() {
+	var err error
+	s, err = discordgo.New("Bot " + *BotToken)
+	if err != nil {
+		log.Fatalf("Invalid bot parameters: %v", err)
+	}
+}
+
+func init() {
+	s.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if h, ok := commandHandlers[i.ApplicationCommandData().Name]; ok {
+			h(s, i)
+		}
+	})
+}
+
+func main() {
+	s.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		log.Printf("Logged in as: %v#%v", s.State.User.Username, s.State.User.Discriminator)
+	})
+	err := s.Open()
+	if err != nil {
+		log.Fatalf("Cannot open the session: %v", err)
+	}
+
+	db, err := sql.Open("sqlite3", "file:locked.sqlite?cache=shared")
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Adding commands...")
+	registeredCommands := make([]*discordgo.ApplicationCommand, len(commands))
+	for i, v := range commands {
+		cmd, err := s.ApplicationCommandCreate(s.State.User.ID, *GuildID, v)
+		if err != nil {
+			log.Panicf("Cannot create '%v' command: %v", v.Name, err)
+		}
+		registeredCommands[i] = cmd
+	}
+
+	defer s.Close()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	log.Println("Press Ctrl+C to exit")
+	<-stop
+
+	log.Println("Removing commands...")
+	for _, v := range registeredCommands {
+		err := s.ApplicationCommandDelete(s.State.User.ID, *GuildID, v.ID)
+		if err != nil {
+			log.Panicf("Cannot delete '%v' command: %v", v.Name, err)
+		}
+	}
+
+	db.Close()
+
+	log.Println("Gracefully shutting down.")
+}
+
+func create(num int64) *int64 {
+	return &num
+}
