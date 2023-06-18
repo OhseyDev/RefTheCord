c364ee5a69c8ee20b200e6ad12183eb5aefaa57a
Ohsey <90527387+OhseyDev@users.noreply.github.com>
2023-06-18 15:43:37 +0100

    Implement

--- commands.go
+++ commands.go
@@ -1,92 +1,20 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"time"
-
 	"github.com/bwmarrin/discordgo"
 )
 
 var (
-	integerOptionMinValue          = 1.0
-	dmPermission                   = false
-	defaultMemberPermissions int64 = discordgo.PermissionManageServer
-
 	commands = []*discordgo.ApplicationCommand{
-		{
-			Name:                     "permission-overview",
-			Description:              "Command for demonstration of default command permissions",
-			DefaultMemberPermissions: &defaultMemberPermissions,
-			DMPermission:             &dmPermission,
-		},
-		{
-			Name:        "options",
-			Description: "Command for demonstrating options",
-			Options: []*discordgo.ApplicationCommandOption{
-
-				{
-					Type:        discordgo.ApplicationCommandOptionString,
-					Name:        "string-option",
-					Description: "String option",
-					Required:    true,
-				},
-				{
-					Type:        discordgo.ApplicationCommandOptionInteger,
-					Name:        "integer-option",
-					Description: "Integer option",
-					MinValue:    &integerOptionMinValue,
-					MaxValue:    10,
-					Required:    true,
-				},
-				{
-					Type:        discordgo.ApplicationCommandOptionNumber,
-					Name:        "number-option",
-					Description: "Float option",
-					MaxValue:    10.1,
-					Required:    true,
-				},
-				{
-					Type:        discordgo.ApplicationCommandOptionBoolean,
-					Name:        "bool-option",
-					Description: "Boolean option",
-					Required:    true,
-				},
-
-				// Required options must be listed first since optional parameters
-				// always come after when they're used.
-				// The same concept applies to Discord's Slash-commands API
-
-				{
-					Type:        discordgo.ApplicationCommandOptionChannel,
-					Name:        "channel-option",
-					Description: "Channel option",
-					// Channel type mask
-					ChannelTypes: []discordgo.ChannelType{
-						discordgo.ChannelTypeGuildText,
-						discordgo.ChannelTypeGuildVoice,
-					},
-					Required: false,
-				},
-				{
-					Type:        discordgo.ApplicationCommandOptionUser,
-					Name:        "user-option",
-					Description: "User option",
-					Required:    false,
-				},
-				{
-					Type:        discordgo.ApplicationCommandOptionRole,
-					Name:        "role-option",
-					Description: "Role option",
-					Required:    false,
-				},
-			},
-		},
 		{
 			Name:                     "support",
 			Description:              "Access guidance and support",
 			DefaultMemberPermissions: create(discordgo.PermissionAll),
 			Options: []*discordgo.ApplicationCommandOption{
+				{
+					Name:        "info",
+					Description: "Support information",
+				},
 				{
 					Name:        "ticket",
 					Description: "Open and manage support tickets",
@@ -107,190 +35,22 @@ var (
 			},
 		},
 		{
-			Name:        "responses",
-			Description: "Interaction responses testing initiative",
-			Options: []*discordgo.ApplicationCommandOption{
-				{
-					Name:        "resp-type",
-					Description: "Response type",
-					Type:        discordgo.ApplicationCommandOptionInteger,
-					Choices: []*discordgo.ApplicationCommandOptionChoice{
-						{
-							Name:  "Channel message with source",
-							Value: 4,
-						},
-						{
-							Name:  "Deferred response With Source",
-							Value: 5,
-						},
-					},
-					Required: true,
-				},
-			},
-		},
-		{
 			Name:        "rules",
 			Description: "Show rules",
+		},
+		{
+			Name:        "submit",
+			Description: "Manually submit a clip",
 		},
 	}
 
 	commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
-		"basic-command": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-				Type: discordgo.InteractionResponseChannelMessageWithSource,
-				Data: &discordgo.InteractionResponseData{
-					Content: "Hey there! Congratulations, you just executed your first slash command",
-				},
-			})
-		},
-		"options": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-			options := i.ApplicationCommandData().Options
-
-			optionMap := make(map[string]*discordgo.ApplicationCommandInteractionDataOption, len(options))
-			for _, opt := range options {
-				optionMap[opt.Name] = opt
-			}
-
-			// This example stores the provided arguments in an []interface{}
-			// which will be used to format the bot's response
-			margs := make([]interface{}, 0, len(options))
-			msgformat := "You learned how to use command options! " +
-				"Take a look at the value(s) you entered:\n"
-
-			// Get the value from the option map.
-			// When the option exists, ok = true
-			if option, ok := optionMap["string-option"]; ok {
-				// Option values must be type asserted from interface{}.
-				// Discordgo provides utility functions to make this simple.
-				margs = append(margs, option.StringValue())
-				msgformat += "> string-option: %s\n"
-			}
-
-			if opt, ok := optionMap["integer-option"]; ok {
-				margs = append(margs, opt.IntValue())
-				msgformat += "> integer-option: %d\n"
-			}
-
-			if opt, ok := optionMap["number-option"]; ok {
-				margs = append(margs, opt.FloatValue())
-				msgformat += "> number-option: %f\n"
-			}
-
-			if opt, ok := optionMap["bool-option"]; ok {
-				margs = append(margs, opt.BoolValue())
-				msgformat += "> bool-option: %v\n"
-			}
-
-			if opt, ok := optionMap["channel-option"]; ok {
-				margs = append(margs, opt.ChannelValue(nil).ID)
-				msgformat += "> channel-option: <#%s>\n"
-			}
-
-			if opt, ok := optionMap["user-option"]; ok {
-				margs = append(margs, opt.UserValue(nil).ID)
-				msgformat += "> user-option: <@%s>\n"
-			}
-
-			if opt, ok := optionMap["role-option"]; ok {
-				margs = append(margs, opt.RoleValue(nil, "").ID)
-				msgformat += "> role-option: <@&%s>\n"
-			}
-
-			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-				// Ignore type for now, they will be discussed in "responses"
-				Type: discordgo.InteractionResponseChannelMessageWithSource,
-				Data: &discordgo.InteractionResponseData{
-					Content: fmt.Sprintf(
-						msgformat,
-						margs...,
-					),
-				},
-			})
-		},
-		"permission-overview": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-			perms, err := s.ApplicationCommandPermissions(s.State.User.ID, i.GuildID, i.ApplicationCommandData().ID)
-
-			var restError *discordgo.RESTError
-			if errors.As(err, &restError) && restError.Message != nil && restError.Message.Code == discordgo.ErrCodeUnknownApplicationCommandPermissions {
-				s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-					Type: discordgo.InteractionResponseChannelMessageWithSource,
-					Data: &discordgo.InteractionResponseData{
-						Content: ":x: No permission overwrites",
-					},
-				})
-				return
-			} else if err != nil {
-				panic(err)
-			}
-
-			if err != nil {
-				panic(err)
-			}
-			format := "- %s %s\n"
-
-			channels := ""
-			users := ""
-			roles := ""
-
-			for _, o := range perms.Permissions {
-				emoji := "❌"
-				if o.Permission {
-					emoji = "☑"
-				}
-
-				switch o.Type {
-				case discordgo.ApplicationCommandPermissionTypeUser:
-					users += fmt.Sprintf(format, emoji, "<@!"+o.ID+">")
-				case discordgo.ApplicationCommandPermissionTypeChannel:
-					allChannels, _ := discordgo.GuildAllChannelsID(i.GuildID)
-
-					if o.ID == allChannels {
-						channels += fmt.Sprintf(format, emoji, "All channels")
-					} else {
-						channels += fmt.Sprintf(format, emoji, "<#"+o.ID+">")
-					}
-				case discordgo.ApplicationCommandPermissionTypeRole:
-					if o.ID == i.GuildID {
-						roles += fmt.Sprintf(format, emoji, "@everyone")
-					} else {
-						roles += fmt.Sprintf(format, emoji, "<@&"+o.ID+">")
-					}
-				}
-			}
-
-			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-				Type: discordgo.InteractionResponseChannelMessageWithSource,
-				Data: &discordgo.InteractionResponseData{
-					Embeds: []*discordgo.MessageEmbed{
-						{
-							Title:       "Permissions overview",
-							Description: "Overview of permissions for this command",
-							Fields: []*discordgo.MessageEmbedField{
-								{
-									Name:  "Users",
-									Value: users,
-								},
-								{
-									Name:  "Channels",
-									Value: channels,
-								},
-								{
-									Name:  "Roles",
-									Value: roles,
-								},
-							},
-						},
-					},
-					AllowedMentions: &discordgo.MessageAllowedMentions{},
-				},
-			})
-		},
 		"support": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 			options := i.ApplicationCommandData().Options
 			content := ""
 			switch options[0].Name {
-			case "subcommand":
-				content = "Subcommand"
+			case "info":
+				content = "Not Implemented"
 			case "ticket":
 				options = options[0].Options
 				switch options[0].Name {
@@ -315,70 +75,21 @@ var (
 				},
 			})
 		},
-		"responses": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-			// Responses to a command are very important.
-			// First of all, because you need to react to the interaction
-			// by sending the response in 3 seconds after receiving, otherwise
-			// interaction will be considered invalid and you can no longer
-			// use the interaction token and ID for responding to the user's request
-
-			content := ""
-			// As you can see, the response type names used here are pretty self-explanatory,
-			// but for those who want more information see the official documentation
-			switch i.ApplicationCommandData().Options[0].IntValue() {
-			case int64(discordgo.InteractionResponseChannelMessageWithSource):
-				content =
-					"You just responded to an interaction, sent a message and showed the original one. " +
-						"Congratulations!"
-				content +=
-					"\nAlso... you can edit your response, wait 5 seconds and this message will be changed"
-			default:
-				err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-					Type: discordgo.InteractionResponseType(i.ApplicationCommandData().Options[0].IntValue()),
-				})
-				if err != nil {
-					s.FollowupMessageCreate(i.Interaction, true, &discordgo.WebhookParams{
-						Content: "Something went wrong",
-					})
-				}
-				return
-			}
-
-			err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-				Type: discordgo.InteractionResponseType(i.ApplicationCommandData().Options[0].IntValue()),
-				Data: &discordgo.InteractionResponseData{
-					Content: content,
-				},
-			})
-			if err != nil {
-				s.FollowupMessageCreate(i.Interaction, true, &discordgo.WebhookParams{
-					Content: "Something went wrong",
-				})
-				return
-			}
-			time.AfterFunc(time.Second*5, func() {
-				content := content + "\n\nWell, now you know how to create and edit responses. " +
-					"But you still don't know how to delete them... so... wait 10 seconds and this " +
-					"message will be deleted."
-				_, err = s.InteractionResponseEdit(i.Interaction, &discordgo.WebhookEdit{
-					Content: &content,
-				})
-				if err != nil {
-					s.FollowupMessageCreate(i.Interaction, true, &discordgo.WebhookParams{
-						Content: "Something went wrong",
-					})
-					return
-				}
-				time.Sleep(time.Second * 10)
-				s.InteractionResponseDelete(i.Interaction)
-			})
-		},
 		"rules": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 				Type: discordgo.InteractionResponseChannelMessageWithSource,
 				Data: &discordgo.InteractionResponseData{
 					Flags:   discordgo.MessageFlagsEphemeral,
 					Content: "TODO: Setup rules",
+				},
+			})
+		},
+		"submit": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Flags:   discordgo.MessageFlagsEphemeral,
+					Content: "TODO: Implement manual submit",
 				},
 			})
 		},
--- main.go
+++ main.go
@@ -12,8 +12,10 @@ import (
 )
 
 var (
-	GuildID  = flag.String("guild", "", "Test guild ID. If not passed - bot registers commands globally")
-	BotToken = flag.String("token", "", "Bot access token")
+	GuildID            = flag.String("guild", "", "Test guild ID. If not passed - bot registers commands globally")
+	BotToken           = flag.String("token", "", "Bot access token")
+	MySQL              = flag.String("mysql", "", "MySQL Database URL")
+	SubmissionsChannel = flag.String("submissions", "", "Submissions channel")
 )
 
 var s *discordgo.Session
@@ -37,17 +39,78 @@ func init() {
 }
 
 func main() {
+	db, err := sql.Open("mysql", *MySQL)
+	if err != nil {
+		log.Panicf("Cannot connect to MySQL database: %v", err)
+	}
+
+	db.Exec("CREATE TABLE Submissions (User varchar(18) NOT NULL, DiscordMsg varchar(), Votes TINYINT(255), PRIMARY KEY (User))")
+	db.Exec("CREATE TABLE UserVotes (User varchar(18) NOT NULL, EventYr YEAR NOT NULL, EventMonth BIT(4), Submission longint, PRIMARY KEY (User))")
+
 	s.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
 		log.Printf("Logged in as: %v#%v", s.State.User.Username, s.State.User.Discriminator)
 	})
-	err := s.Open()
-	if err != nil {
-		log.Fatalf("Cannot open the session: %v", err)
-	}
 
-	db, err := sql.Open("mysql", "file:locked.sqlite?cache=shared")
+	s.AddHandler(func(s *discordgo.Session, react *discordgo.MessageReactionAdd) {
+		if react.Emoji.Name != "white_check_mark" {
+			return
+		}
+		msg, err := s.ChannelMessage(react.ChannelID, react.MessageID)
+		if err != nil {
+			log.Printf("Failed to obtain Discord message")
+			return
+		}
+		if msg.Author.ID == react.UserID {
+			return
+		}
+	})
+
+	s.AddHandler(func(s *discordgo.Session, react *discordgo.MessageReactionRemove) {
+		if react.Emoji.Name != "white_check_mark" {
+			return
+		}
+		msg, err := s.ChannelMessage(react.ChannelID, react.MessageID)
+		if err != nil {
+			log.Printf("Failed to obtain Discord message")
+			return
+		}
+		if msg.Author.ID == react.UserID {
+			return
+		}
+	})
+
+	s.AddHandler(func(s *discordgo.Session, c *discordgo.MessageCreate) {
+		if c.ChannelID != *SubmissionsChannel {
+			return
+		}
+		_, err := db.Query("SELECT COUNT(1) DiscordMsg FROM Submissions WHERE User='%s'", c.Author.ID)
+		if err != nil {
+			log.Printf("Failed to execute query: %r", err)
+			return
+		}
+		s.ChannelMessageDelete(c.ChannelID, c.Message.ID)
+	})
+
+	s.AddHandler(func(s *discordgo.Session, d *discordgo.MessageDelete) {
+		if d.ChannelID != *SubmissionsChannel {
+			return
+		}
+		id, err := db.Query("SELECT COUNT(1) DiscordMsg FROM Submissions WHERE User='%s'", d.Author.ID)
+		if err != nil {
+			log.Printf("Failed to execute query: %r", err)
+			return
+		}
+		loggedId := ""
+		id.Scan(&loggedId)
+		if loggedId == d.Message.ID {
+			db.Exec("DELETE FROM Submissions WHERE User='%s'", d.Author.ID)
+		}
+	})
+
+	err = s.Open()
 	if err != nil {
-		panic(err)
+		db.Close()
+		log.Fatalf("Cannot open the session: %v", err)
 	}
 
 	log.Println("Adding commands...")
@@ -74,9 +137,7 @@ func main() {
 			log.Panicf("Cannot delete '%v' command: %v", v.Name, err)
 		}
 	}
-
 	db.Close()
-
 	log.Println("Gracefully shutting down.")
 }
 
