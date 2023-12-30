70c535a344a3897dd5be46c860eef41436fa4823
Ohsey <admin@ohsey.me>
2023-12-30 08:08:53 +0000

    Fix bug with program

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/bwmarrin/discordgo v0.27.1 // indirect
+	github.com/caarlos0/env/v10 v10.0.0 // indirect
 	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
--- main.go
+++ main.go
@@ -2,44 +2,53 @@ package main
 import (
 	"github.com/bwmarrin/discordgo"
 	"fmt"
-	"flag"
+	"github.com/caarlos0/env/v10"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 	"os"
+	"os/signal"
 	"log"
+	"syscall"
+	"time"
 )
 
-var (
-	Token string
-	DBConnect string
-)
+type config struct {
+	Token		string	`env:"TOKEN"`
+	ConnectStr	string	`env:"DBCONN"`
+}
 
 func main() {
-	flag.StringVar(&Token, "t", "", "Bot Token")
-	flag.StringVar(&DBConnect, "db", "", "Database Connect String")
-	flag.Parse()
-	if Token == nil || len(Token) <= 0 {
-		Token = os.Getenv("BOT_TOKEN")
-		if Token == nil {
-			log.Fatalf("No Discord Bot Token provided")
-			return
-		}
-	}
-	discord, err := discordgo.New("Bot " + TOKEN)
+	cfg := config{}
+	if err := env.Parse(&cfg); err != nil { fmt.Printf("%+v\n", err) }
+	db, err := sql.Open("mysql", cfg.ConnectStr)
 	if err != nil {
-		log.Fatalf("Error on Discord login: " + err)
+		log.Fatalf("Failed to connect to database: %v", err)
 		return
 	}
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+	discord, err := discordgo.New("Bot " + cfg.Token)
+	if err != nil {
+		log.Fatalf("Error on Discord login: %v", err)
+		return
+	}
+	discord.Identify.Intents = 17995913063488
 	discord.AddHandler(messageCreate)
+	err = discord.Open()
+	if err != nil {
+		log.Fatalf("Cannot open the session: %v", err)
+		return
+	}
+	defer discord.Close()
 	log.Print("Bot is now running. Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
-	discord.Close()
 }
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.Author.ID == s.State.UserID {
-		return
-	}
-	chanid := m.ChannelID
+	if m.Author.Bot { return }
+	// chanid := m.ChannelID
 }
 
